a3e7caa4c7f9de3d66097a5da63f4863219805aa
Rachael Orben <raorben@gmail.com>
2023-08-31 11:47:26 -0700

    //

--- 1_MOSAIC_seabird_QAQC.R
+++ 1_MOSAIC_seabird_QAQC.R
@@ -20,12 +20,14 @@ library(lubridate)
 
 if(Sys.info()[7]=="rachaelorben") {usr<-"/Users/rachaelorben";
   dir<-"/Library/CloudStorage/Box-Box/Seabird Oceanography Lab/Current_Research/MOSAIC_Seabird At-Sea Observations/"}
-if(Sys.info()[7]=="kennerlw") dir<-"/Volumes/GoogleDrive/My Drive/Seabird_Oceanography_Lab/At-SeaSurveys/HALO/Raw Dat"
 
-sp<-read.csv(paste0(usr,dir,"data/SeaLog-Species_CodeList.csv"))
+if(Sys.info()[7]=="kennerlw") {usr<-"//Users//kennerlw";
+dir<-"//Library//CloudStorage//Box-Box//Seabird Oceanography Lab//Current_Research//MOSAIC_Seabird At-Sea Observations//"}
+
+sp<-read.csv(paste0(usr,dir,"data//SeaLog-Species_CodeList.csv"))
 names(sp)<-c("Species_Name","Code","Sci_name","Animal")
 
-Files<-list.files(paste0(kennerlw,dir,"data/Corrected"),pattern = ".csv",full.names = T,recursive = T)
+Files<-list.files(paste0(usr,dir,"data//Corrected"),pattern = ".csv",full.names = T,recursive = T)
 
 survey_dat<-NULL
 for (j in 1:length(Files)){
