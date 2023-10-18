b2f6f8a263db4c505404490f6b2a7e8e446a2709
Will K <william.kennerley@oregonstate.edu>
2023-10-18 14:14:33 -0700

    Additional minor edits to script to get October 2023 data proofed

--- 1_MOSAIC_seabird_QAQC.R
+++ 1_MOSAIC_seabird_QAQC.R
@@ -18,34 +18,33 @@ library(dplyr)
 library(data.table)
 library(lubridate)
 
-#if(Sys.info()[7]=="rachaelorben") {usr<-"/Users/rachaelorben";
+if(Sys.info()[7]=="rachaelorben") {usr<-"/Users/rachaelorben";
   dir<-"/Library/CloudStorage/Box-Box/Seabird Oceanography Lab/Current_Research/MOSAIC_Seabird At-Sea Observations/"}
 
 if(Sys.info()[7]=="kennerlw") {usr<-"/Users/kennerlw";
 dir<-"/Box/Seabird Oceanography Lab/Current_Research/MOSAIC_Seabird At-Sea Observations/"}
 
-
 sp<-read.csv(paste0(usr,dir,"data/SeaLog-Species_CodeList.csv"))
-
 names(sp)<-c("Species_Name","Code","Sci_name","Animal")
 
-Files<-list.files(paste0(usr,dir,"data/Corrected"),pattern = ".csv",full.names = T,recursive = T)
+Files<-list.files(paste0(usr,dir,"data/CorrectedData"),pattern = ".csv",full.names = T,recursive = T)
 
 survey_dat<-NULL
 for (j in 1:length(Files)){
-  dat<-read.csv(file=Files[j],stringsAsFactors=FALSE,na.strings = "null") 
+  dat<-read.csv(file=Files[j],stringsAsFactors=FALSE,na.strings = "NA") 
   dat$TripID<-as.character(dat$TripID)
   dat$PortCondition<-as.character(dat$PortCondition)
   dat$Beaufort<-as.character(dat$Beaufort)
   dat$Record<-as.character(dat$Record)
   dat$oid<-1:nrow(dat)
   dat$file<-Files[j]
-  
-  dat$Cruise_ID<-sapply(strsplit(Files[j], split='/', fixed=TRUE), function(x) (x[12])) #RAO specifications
   dat$Transect<-as.character(dat$Transect)
-  
+  #dat$Cruise_ID<-sapply(strsplit(Files[j], split='\', fixed=TRUE), function(x) (x[12])) #RAO specifications
+  dat$Cruise_ID<-sapply(strsplit(Files[j], split='/', fixed=TRUE), function(x) (x[10])) #WLK specifications
   survey_dat<-bind_rows(survey_dat,dat)
 }
+
+survey_dat
 
 survey_dat$Vessel[survey_dat$Vessel=="SHIMADA"]<-"Shimada"
 survey_dat$Vessel[survey_dat$Vessel=="S"]<-"Shimada"
@@ -74,7 +73,7 @@ survey_dat<-left_join(survey_dat, sp, by=c("Species"="Code"))
 
 # searches for sightings without number of birds --------------------------
 unique(survey_dat$Cruise_ID)
-C_ID<-unique(survey_dat$Cruise_ID)[4] #select new cruise ID here [#]
+C_ID<-unique(survey_dat$Cruise_ID)[5] #select new cruise ID here [#]
 
 missing_number<-survey_dat%>%filter(Cruise_ID==C_ID) %>%
   filter(is.na(Species)==FALSE)%>%
@@ -83,7 +82,7 @@ missing_number #should say: <0 rows> (or 0-length row.names)
 
 #ADD YES TO THIS FILE NAME ONCE YOU HAVE FIXED THESE SIGHTINGS so it is not written over!!!!
 #write.csv(missing_number, 
-#          paste0(usr,dir,"Analysis/processed_data/",C_ID,"_sightings_missing_count_correctedYES.csv"))
+          #paste0(usr,dir,"Analysis/processed_data/",C_ID,"_sightings_missing_count_correctedYES.csv"))
 
 # species summary for new cruise ---------------------------------------------------------
 species_sum<-survey_dat%>%filter(Cruise_ID==C_ID)%>% 
@@ -97,12 +96,12 @@ species_sum_mammals<-species_sum%>%filter(Animal=="mammal")
 species_sum_birds<-species_sum%>%filter(Animal=="bird")%>%
   arrange(Species)
 
-# write.csv(species_sum_birds%>%filter(On.OffTx=="ON")%>%
+#write.csv(species_sum_birds%>%filter(On.OffTx=="ON")%>%
 #             ungroup()%>%
 #             select(-Animal,-On.OffTx), 
 #           paste0(usr,dir,"Analysis/processed_data/",C_ID,"_SpeciesSummaryTable_allbirdsON.csv")) #birds ON
-# 
-# write.csv(species_sum_birds%>%filter(On.OffTx=="OFF")%>%
+ 
+#write.csv(species_sum_birds%>%filter(On.OffTx=="OFF")%>%
 #             ungroup()%>%
 #             select(-Animal,-On.OffTx), 
 #           paste0(usr,dir,"Analysis/processed_data/",C_ID,"_SpeciesSummaryTable_allbirdsOFF.csv")) #birds OFF
@@ -112,6 +111,7 @@ species_sum_birds<-species_sum%>%filter(Animal=="bird")%>%
 species_sum_UNK<-species_sum%>%filter(is.na(Animal)==TRUE) #codes to fix in data
 info<-survey_dat %>%
   filter(Species %in% species_sum_UNK$Species)
+info
 
 species_sum_UNK_dt<-full_join(species_sum_UNK,info%>%select(Species,datetime,StarboardObs,PortObs), by="Species")%>%
   filter(is.na(Species)==FALSE)%>%ungroup()%>%
@@ -120,7 +120,7 @@ species_sum_UNK_dt<-full_join(species_sum_UNK,info%>%select(Species,datetime,Sta
 species_sum_UNK_dt
 
 # summary table of sightings to fix, added FIXED once these are made
-# write.csv(species_sum_UNK_dt,paste0(usr,dir,"Analysis/processed_data/",C_ID,"_SpeciesSummaryTable_BIRD_tofix.csv"))
+#write.csv(species_sum_UNK_dt,paste0(usr,dir,"Analysis/processed_data/",C_ID,"_SpeciesSummaryTable_BIRD_tofix.csv"))
 
 #  makes Obs column & Obs side column -------------------------------------
 names(survey_dat)
@@ -154,12 +154,18 @@ names(survey_dat)
 
 flight_heights<-survey_dat%>%filter(is.na(UserSelect1)==FALSE)
 
-sp_SUM_fh<-flight_heights%>%group_by(Species,PrimaryBehavior,SecondaryBehavior,UserSelect1)%>%
+sp_SUM_fh<-flight_heights%>%group_by(Species,PrimaryBehavior,SecondaryBehavior,UserSelect1,datetime)%>%
   summarise(n=n())
 
 sp_SUM_fh%>%ungroup()%>%filter(PrimaryBehavior=="Sitting")
 
 unique(sp_SUM_fh$PrimaryBehavior)    
+
+
+
+
+
+
 
 
 # removes columns that aren't collected & Obs columns-----------------------------------
